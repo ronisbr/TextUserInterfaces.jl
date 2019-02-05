2229b7da444e5b20cf3730f77590e3f4733459d8
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2019-02-05 21:18:14 -0200

    :rotating_light: Add tests related to NCurses version

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,27 @@ using TextUserInterfaces
 
 init_tui()
 
-@test TextUserInterfaces.ncurses.libncurses != C_NULL
-@test TextUserInterfaces.ncurses.libpanel   != C_NULL
-@test TextUserInterfaces.ncurses.libmenu    != C_NULL
-@test TextUserInterfaces.ncurses.libform    != C_NULL
+# Check if the libraries were loaded
+# ==============================================================================
+
+@testset "Libraries loading..." begin
+    @test TextUserInterfaces.ncurses.libncurses != C_NULL
+    @test TextUserInterfaces.ncurses.libpanel   != C_NULL
+    @test TextUserInterfaces.ncurses.libmenu    != C_NULL
+    @test TextUserInterfaces.ncurses.libform    != C_NULL
+end
+
+# Check if the NCurses version is consistent
+# ==============================================================================
+
+@testset "NCurses version..." begin
+    ncurses_version = curses_version()
+
+    @test haskey(ncurses_version, :major) == true
+    @test haskey(ncurses_version, :minor) == true
+    @test haskey(ncurses_version, :patch) == true
+    @test ncurses_version.major           >= 5
+
+end
 
 destroy_tui()
