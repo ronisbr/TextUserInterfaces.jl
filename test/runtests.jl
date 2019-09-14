6ab7b96525813eb859c541e9626a93b2a085eed7
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2019-09-14 10:04:34 -0300

    :rotating_light: Add test related to resizeterm

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,26 @@
 using Test
 using TextUserInterfaces
 
-init_tui()
-
 # Check if the libraries were loaded
 # ==============================================================================
 
 @testset "Libraries loading..." begin
+    init_tui()
+
     @test TextUserInterfaces.ncurses.libncurses != C_NULL
     @test TextUserInterfaces.ncurses.libpanel   != C_NULL
     @test TextUserInterfaces.ncurses.libmenu    != C_NULL
     @test TextUserInterfaces.ncurses.libform    != C_NULL
+
+    destroy_tui()
 end
 
 # Check if the NCurses version is consistent
 # ==============================================================================
 
 @testset "NCurses version..." begin
+    init_tui()
+
     ncurses_version = curses_version()
 
     @test haskey(ncurses_version, :major) == true
@@ -24,6 +28,19 @@ end
     @test haskey(ncurses_version, :patch) == true
     @test ncurses_version.major           >= 5
 
+    destroy_tui()
 end
 
-destroy_tui()
+# Check if terminal resizing is working
+# ==============================================================================
+
+@testset "Terminal resizing..." begin
+    init_tui()
+
+    resizeterm(25,80)
+
+    @test LINES() == 25
+    @test COLS()  == 80
+
+    destroy_tui()
+end
