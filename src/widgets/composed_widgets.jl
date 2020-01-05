ba5086dda7329258b8a0c698fb2cc3a38acd140c
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-01-05 12:56:48 -0300

    :wrench: Improve the destruction of composed widgets

--- src/widgets/composed_widgets.jl
+++ src/widgets/composed_widgets.jl
@@ -16,6 +16,33 @@
 ################################################################################
 
 accept_focus(widget::ComposedWidget) = accept_focus(widget.container)
+
+function destroy_widget(widget::ComposedWidget; refresh::Bool = true)
+    @unpack common, container = widget
+    @unpack parent, buffer = common
+
+    widget_desc = obj_desc(widget)
+
+    # First, we need to delete all children widgets.
+    while length(container.widgets) > 0
+        w = pop!(container.widgets)
+        destroy_widget(w, refresh = false)
+    end
+
+    delwin(buffer)
+    buffer = Ptr{WINDOW}(0)
+
+    # Remove the widget from the parent.
+    remove_widget(parent, widget)
+
+    @log info "destroy_widget" "Widget $widget_desc destroyed."
+
+    # If required, perform a full refresh of the parent window.
+    refresh && refresh_window(parent; force_redraw = true)
+
+    return nothing
+end
+
 process_focus(widget::ComposedWidget, k::Keystroke) = process_focus(widget.container, k)
 redraw(widget::ComposedWidget) = redraw(widget.container)
 release_focus(widget::ComposedWidget) = release_focus(widget.container)
