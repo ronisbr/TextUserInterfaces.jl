a877f852f1ec19da0da1ebf3b1ac05b10e73ef61
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-06-22 23:30:21 -0300

    :rotating_light: Fix tests

--- test/log_test/model.jl
+++ test/log_test/model.jl
@@ -12,19 +12,22 @@ function log_test()
     p0 = ncurses_color(bold = false)
     p1 = ncurses_color(:black, :white)
 
-    w,c = create_window_with_container(border = true, title = " WINDOW ",
-                                       top = 2, left = 2, width = 56, height = 10)
-    ~   = create_widget(Val(:label), c;
-                        anchor_top = Anchor(c, :top, 0),   height = 1,
-                        anchor_left = Anchor(c, :left, 0), width  = 40,
+    opc = newopc(top = 2, left = 2, width = 56, height = 10)
+    w,c = create_window_with_container(opc; border = true, title = " WINDOW ")
+
+    opc = newopc(anchor_top  = Anchor(c, :top, 0),  height = 1,
+                 anchor_left = Anchor(c, :left, 0), width  = 40)
+    ~   = create_widget(Val(:label), c, opc;
                         text = "This is a label", color = p0)
-    bt  = create_widget(Val(:button), c;
-                        anchor_middle = Anchor(c, :middle, 0),
-                        anchor_left = Anchor(c, :left, 0), width = 12,
+
+    opc = newopc(anchor_middle = Anchor(c, :middle, 0),
+                 anchor_left   = Anchor(c, :left, 0), width = 12)
+    bt  = create_widget(Val(:button), c, opc;
                         label = "Button 1", color = p0, color_highlight = p1)
-    pb  = create_widget(Val(:progress_bar), c;
-                         anchor_bottom = Anchor(c, :bottom, 0), width = 40,
-                         anchor_left = Anchor(c, :left, 0), value = 100)
+
+    opc = newopc(anchor_bottom = Anchor(c, :bottom, 0), width = 40,
+                 anchor_left   = Anchor(c, :left, 0))
+    pb  = create_widget(Val(:progress_bar), c, opc; value = 100)
 
     # Focus manager.
     tui.focus_chain = [w]
