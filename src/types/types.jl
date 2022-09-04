f4b24bbcbe65e64c65ad0f5fc8d371ccbcc22192
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-09-04 15:23:41 -0300

    :wrench: Multiple fixes

--- src/types/types.jl
+++ src/types/types.jl
@@ -7,19 +7,24 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+include("./colors.jl")
+include("./logger.jl")
+include("./themes.jl")
+include("./windows.jl")
+
 @with_kw mutable struct TextUserInterface
     initialized::Bool = false
 
     # Windows
     # ==========================================================================
     stdscr::Ptr{WINDOW} = Ptr{WINDOW}(0)
+
+    # Colors
+    # ==========================================================================
+    initialized_color_pair::Vector{Tuple{Int, Int}} = Tuple{Int, Int}[]
+
+    # Default theme
+    # ==========================================================================
+
+    default_theme::Theme = Theme()
 end
-
-# Global instance to store the text user interface configurations.
-const tui = TextUserInterface()
-
-# Include of other files that define types
-# ==============================================================================
-
-include("./colors.jl")
-include("./logger.jl")
