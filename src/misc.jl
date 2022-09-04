96ce8642c4f28c792b7572a866438647c615ac93
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-09-04 18:53:55 -0300

    :sparkles: Add object supertype

---
+++ src/misc.jl
@@ -0,0 +1,44 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Description
+# ==============================================================================
+#
+#   Miscellaneous functions.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+export obj_desc, obj_to_ptr
+
+const _test_hide_address = [false]
+
+"""
+    obj_desc(obj)
+
+Return a string with the description of the object `obj` formed by:
+
+    <Object type> (<Object address if mutable>)
+"""
+function obj_desc(obj)
+    obj_type = string(typeof(obj))
+
+    if isimmutable(obj)
+        return obj_type
+    else
+        return obj_type * " (" * obj_to_ptr(obj) * ")"
+    end
+end
+
+"""
+     function obj_to_ptr(obj)
+
+Returns the hexadecimal representation of the address of the object `obj`. It
+**only** works with mutable objects.  If `obj` is immutable, then `0x0` will be
+returned.
+"""
+function obj_to_ptr(obj)
+    if _test_hide_address[1]
+        return "0x0"
+    else
+        return isimmutable(obj) ? "0x0" : repr(UInt64(pointer_from_objref(obj)))
+    end
+end
