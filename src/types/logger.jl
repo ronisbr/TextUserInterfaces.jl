09301a512f9ec9c9f38f9e016f886ee187689b61
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-09-03 21:09:25 -0300

    :sparkles: Add initialization, color support, and logger

---
+++ src/types/logger.jl
@@ -0,0 +1,31 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Description
+# ==============================================================================
+#
+#   Types related to the logger.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+export logger, CRITICAL, WARNING, INFO, VERBOSE
+
+const CRITICAL = 0
+const WARNING  = 1
+const INFO     = 2
+const VERBOSE  = 3
+
+@with_kw mutable struct Logger
+    enabled::Bool = false
+    level::Int = CRITICAL
+    logfile::AbstractString = "./tui.log"
+    timestamp::Bool = true
+
+    # Internals
+    # ==========================================================================
+    file::Union{Nothing, IOStream} = nothing
+    pad::Int = 0
+    lock::Threads.SpinLock = Threads.SpinLock()
+end
+
+# Global logger instance.
+const logger = Logger()
