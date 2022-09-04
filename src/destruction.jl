80539e06fa14dc7d3e30cf6045018c766c2d3407
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-09-04 18:54:30 -0300

    :sparkles: Add windows

--- src/destruction.jl
+++ src/destruction.jl
@@ -15,18 +15,22 @@ export destroy_tui
 Destroy the text user interface (TUI).
 """
 function destroy_tui()
-    @log info "destroy_tui" "TUI will be destroyed."
+    @log INFO "destroy_tui" "TUI will be destroyed."
 
-    if tui.init
+    if tui.initialized
+        @log INFO "destroy_tui" "TUI destroying windows."
+        destroy_all_windows()
+        endwin()
+
         # Mark the TUI as not initialized.
         tui.stdscr = Ptr{WINDOW}(0)
-        tui.init = false
+        tui.initialized = false
     end
 
-    @log info "destroy_tui" "TUI has been destroyed."
+    @log INFO "destroy_tui" "TUI has been destroyed."
 
     # Reset all colors definitions.
-    _reset_color_dir()
+    _reset_color_dict()
 
     # Close log.
     if logger.file !== nothing
