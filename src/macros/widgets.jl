d439c9b20b29c73e3ba45693b1982449921e8ae4
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-09-14 15:25:46 -0300

    :wrench: More improvements

--- src/macros/widgets.jl
+++ src/macros/widgets.jl
@@ -7,7 +7,143 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-export @widget
+export @create_widget_helper, @widget
+
+const _list_layout_args = (
+    :anchor_bottom,
+    :anchor_left,
+    :anchor_right,
+    :anchor_top,
+    :anchor_center,
+    :anchor_middle,
+    :top,
+    :left,
+    :height,
+    :width
+)
+
+const _list_layout_anchors = (
+    :anchor_bottom,
+    :anchor_left,
+    :anchor_right,
+    :anchor_top,
+    :anchor_center,
+    :anchor_middle
+)
+
+"""
+    @create_widget_helper(widget_symbol[, macro_name])
+
+This macro defines a helper to create a widget of type `widget_symbol`. The
+helper name is `macro_name`. If the latter is not available, then it will be
+composed of the prefix `tui_` plus the `widget_symbol`.
+"""
+macro create_widget_helper(widget_symbol)
+    macro_name = Symbol("tui_" * string(widget_symbol))
+    return esc(:(@create_widget_helper $widget_symbol $macro_name))
+end
+
+macro create_widget_helper(widget_symbol, macro_name)
+    mnq = Meta.quot(macro_name)
+    wsq = Meta.quot(widget_symbol)
+
+    expr = quote
+"""
+    @$($mnq)(args...)
+
+This macro creates a widget of type `$($wsq)` using the arguments
+`args...`.
+
+`args` must be a set of expressions as follows:
+
+    <parameter> = <value>
+
+If the parameter `parent` is present, then the created widget will be added to
+`parent`. Otherwise, the widget will be created and returned, but not added to
+any parent.
+"""
+        macro $macro_name(args...)
+            # List of expressions to be passed to the keyword as arguments.
+            expr_kwargs = Expr[]
+
+            # List of expressions to be passed to the creation of
+            # `ObjectLayout`.
+            expr_layout = Expr[]
+
+            # List of expressions to connect signals.
+            expr_signals = Expr[]
+
+            # Variables to verify if the `parent` was defined. In this case, the
+            # created widget will be added to it.
+            has_parent = false
+            parent = nothing
+
+            ws = Meta.quot($wsq)
+
+            for a in args
+                # `a` must be an expression like `property = value`.
+                if (a isa Expr) && (a.head === :(=))
+                    if a.args[1] === :parent
+                        has_parent = true
+                        parent = a.args[2]
+
+                    elseif a.args[1] ∈ _list_layout_args
+                        # In this case, if the parameter is an anchor and value
+                        # is a `Tuple`, we should convert to an `Anchor`.
+                        if (a.args[1] ∈ _list_layout_anchors) &&
+                            (a.args[2] isa Expr) &&
+                            (a.args[2].head === :tuple)
+
+                            v = :(Anchor($(a.args[2].args...)))
+                        else
+                            v = a.args[2]
+                        end
+
+                        push!(expr_layout, Expr(:kw, a.args[1], v))
+
+                    elseif a.args[1] == :signal
+                        # Those statements will be executed after the widget has
+                        # been created. The widget variable name in the
+                        # following code is `widget`.
+                        push!(
+                            expr_signals,
+                            :(@connect(widget, $(a.args[2].args...)))
+                        )
+
+                    else
+                        push!(expr_kwargs, Expr(:kw, a.args[1], a.args[2]))
+                    end
+                end
+            end
+
+            # Assemble the expression that will create the widget.
+            if !has_parent
+                create_widget_expr = quote
+                    layout = ObjectLayout($(expr_layout...))
+                    widget = create_widget(Val($ws), layout; $(expr_kwargs...))
+                    $(expr_signals...)
+                    widget
+                end
+
+            else
+                create_widget_expr = quote
+                    layout = ObjectLayout($(expr_layout...))
+                    widget = create_widget(Val($ws), layout; $(expr_kwargs...))
+                    add_widget!($parent, widget)
+                    $(expr_signals...)
+                    widget
+                end
+            end
+
+            return esc(create_widget_expr)
+        end
+    end
+
+    # Expression to export the macro.
+    expr_export = Expr(:export, Symbol("@", macro_name))
+
+    return Expr(:block, esc(expr), expr_export)
+end
 
 """
     @widget(ex)
@@ -61,3 +197,4 @@ macro widget(ex)
 
     return esc(ret)
 end
+
