5b31fd3b35016e9e141ae6ff2c5a9549520d9bab
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-09-24 20:06:22 -0300

    :wrench: Update the examples

--- examples/all_widgets.jl
+++ examples/all_widgets.jl
@@ -26,9 +26,9 @@ function all_widgets()
 
     label = @tui_label(
         parent      = c,
+        text        = "This is a label.",
         left_anchor = (:parent, :left),
-        top_anchor  = (:parent, :top),
-        text        = "This is a label."
+        top_anchor  = (:parent, :top)
     )
 
     # ANSI Label
@@ -36,9 +36,9 @@ function all_widgets()
 
     ansi_label = @tui_ansi_label(
         parent      = c,
+        text        = "\e[1mThis \e[34mis a color \e[47mlabel.",
         left_anchor = (:parent, :left),
-        top_anchor  = (label, :bottom),
-        text        = "\e[1mThis \e[34mis a color \e[47mlabel."
+        top_anchor  = (label, :bottom)
     )
 
     # Button
@@ -46,23 +46,23 @@ function all_widgets()
 
     button_1 = @tui_button(
         parent      = c,
+        style       = :simple,
         left_anchor = (:parent, :left),
-        top_anchor  = (ansi_label, :bottom, 1),
-        style       = :simple
+        top_anchor  = (ansi_label, :bottom, 1)
     )
 
     button_2 = @tui_button(
         parent        = c,
+        style         = :boxed,
         left_anchor   = (button_1, :right),
-        middle_anchor = (button_1, :middle),
-        style         = :boxed
+        middle_anchor = (button_1, :middle)
     )
 
     button_3 = @tui_button(
         parent        = c,
+        style         = :none,
         left_anchor   = (button_2, :right),
-        middle_anchor = (button_2, :middle),
-        style         = :none
+        middle_anchor = (button_2, :middle)
     )
 
     # Input Fields
@@ -70,23 +70,44 @@ function all_widgets()
 
     input_field_1 = @tui_input_field(
         parent      = c,
+        style       = :simple,
         left_anchor = (:parent, :left),
-        top_anchor  = (button_2, :bottom),
-        style       = :simple
+        top_anchor  = (button_2, :bottom)
     )
 
     input_field_2 = @tui_input_field(
         parent      = c,
+        style       = :boxed,
         left_anchor = (:parent, :left),
-        top_anchor  = (input_field_1, :bottom),
-        style       = :boxed
+        top_anchor  = (input_field_1, :bottom)
     )
 
     input_field_3 = @tui_input_field(
         parent      = c,
+        style       = :none,
         left_anchor = (:parent, :left),
-        top_anchor  = (input_field_2, :bottom),
-        style       = :none
+        top_anchor  = (input_field_2, :bottom)
+    )
+
+    # List Box
+    # ======================================================================================
+
+    list_box_1 = @tui_list_box(
+        parent          = c,
+        data            = ["Item #$i" for i in 1:10],
+        number_of_lines = 5,
+        left_anchor     = (:parent, :left),
+        top_anchor      = (input_field_3, :bottom),
+    )
+
+    list_box_2 = @tui_list_box(
+        parent             = c,
+        data               = ["Item #$i" for i in 1:10],
+        multiple_selection = true,
+        number_of_lines    = 5,
+        show_icon          = true,
+        left_anchor        = (list_box_1, :right),
+        top_anchor         = (list_box_1, :top),
     )
 
     # Bottom Window
