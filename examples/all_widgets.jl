0af97159464cb9e6bc21e2c7b40a9ff17c57b6f0
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-09-24 21:54:17 -0300

    :sparkles: Add public functions for the list box

--- examples/all_widgets.jl
+++ examples/all_widgets.jl
@@ -44,25 +44,59 @@ function all_widgets()
     # Button
     # ======================================================================================
 
+    function button_return_pressed(w; new_label, label_widget)
+        change_text!(label_widget, new_label)
+        return nothing
+    end
+
     button_1 = @tui_button(
         parent      = c,
         style       = :simple,
         left_anchor = (:parent, :left),
-        top_anchor  = (ansi_label, :bottom, 1)
+        top_anchor  = (ansi_label, :bottom, 1),
     )
 
     button_2 = @tui_button(
         parent        = c,
         style         = :boxed,
         left_anchor   = (button_1, :right),
-        middle_anchor = (button_1, :middle)
+        middle_anchor = (button_1, :middle),
     )
 
     button_3 = @tui_button(
         parent        = c,
         style         = :none,
         left_anchor   = (button_2, :right),
-        middle_anchor = (button_2, :middle)
+        middle_anchor = (button_2, :middle),
+    )
+
+    button_information = @tui_label(
+        parent        = c,
+        text          = "",
+        left_anchor   = (button_3, :right, 2),
+        right_anchor  = (:parent, :right),
+        middle_anchor = (button_3, :middle),
+    )
+
+    @connect(
+        button_1,
+        return_pressed,
+        button_return_pressed,
+        (new_label = "Button #1 was pressed.", label_widget = button_information)
+    )
+
+    @connect(
+        button_2,
+        return_pressed,
+        button_return_pressed,
+        (new_label = "Button #2 was pressed.", label_widget = button_information)
+    )
+
+    @connect(
+        button_3,
+        return_pressed,
+        button_return_pressed,
+        (new_label = "Button #3 was pressed.", label_widget = button_information)
     )
 
     # Input Fields
@@ -92,6 +126,14 @@ function all_widgets()
     # List Box
     # ======================================================================================
 
+    function list_box_return_pressed(w; label_widget)
+        current_item   = get_current_item(w)
+        selected_items = get_selected_items(w)
+        str = "Current item: $(current_item), Selected items: $(selected_items)."
+        change_text!(label_widget, str)
+        return nothing
+    end
+
     list_box_1 = @tui_list_box(
         parent          = c,
         data            = ["Item #$i" for i in 1:10],
@@ -100,14 +142,44 @@ function all_widgets()
         top_anchor      = (input_field_3, :bottom),
     )
 
+    list_box_1_information = @tui_label(
+        parent        = c,
+        text          = "",
+        left_anchor   = (list_box_1, :right, 2),
+        right_anchor  = (:parent, :right),
+        middle_anchor = (list_box_1, :middle)
+    )
+
+    @connect(
+        list_box_1,
+        return_pressed,
+        list_box_return_pressed,
+        (; label_widget = list_box_1_information)
+    )
+
     list_box_2 = @tui_list_box(
         parent             = c,
         data               = ["Item #$i" for i in 1:10],
         multiple_selection = true,
         number_of_lines    = 5,
         show_icon          = true,
-        left_anchor        = (list_box_1, :right),
-        top_anchor         = (list_box_1, :top),
+        left_anchor        = (list_box_1, :left),
+        top_anchor         = (list_box_1, :bottom),
+    )
+
+    list_box_2_information = @tui_label(
+        parent        = c,
+        text          = "",
+        left_anchor   = (list_box_2, :right, 2),
+        right_anchor  = (:parent, :right),
+        middle_anchor = (list_box_2, :middle)
+    )
+
+    @connect(
+        list_box_2,
+        return_pressed,
+        list_box_return_pressed,
+        (; label_widget = list_box_2_information)
     )
 
     # Bottom Window
