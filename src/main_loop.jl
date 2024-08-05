97180d26fa5bb0308e6a83bc687799078ee1bd32
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-08-05 13:19:34 -0300

    :sparkles: Add app_main_loop_iteration

--- src/main_loop.jl
+++ src/main_loop.jl
@@ -10,6 +10,12 @@ export app_main_loop
     app_main_loop() -> Nothing
 
 Start the application main loop.
+
+# Keywords
+
+- `exitkey::Symbol`: A symbol that defines the key that leads to the application
+    termination.
+    (**Default**: `:F1`)
 """
 function app_main_loop(; exitkey = :F1)
     # If there is no window in focus, try to acquire it.
@@ -20,29 +26,49 @@ function app_main_loop(; exitkey = :F1)
 
     # Main application loop.
     while true
-        k = getkey()
-        @emit tui keypressed (; keystroke = k)
-
-        # If the signal destroyed the TUI, we should just exit.
-        !tui.initialized && break
-
-        # Check if the keystroke must be passed or if the signal hijacked it.
-        if !@get_signal_property(tui, keypressed, block, false)
-            if _check_keystroke(k, exitkey)
-                break
-            else
-                process_keystroke(k)
-            end
-        end
-
-        @delete_signal_property tui keypressed block
-
-        tui_update()
+        app_main_loop_iteration(; block = false, exitkey) || break
     end
 
     destroy_tui()
 
     return nothing
+end
+
+"""
+    app_main_loop_iteration(; kwargs...) -> Bool
+
+Execute one iteration of the application main loop. If this funciton returns `true`, it
+means that the should continue the iterations. Otherwise, we must quit the application.
+
+# Keywords
+
+- `block::Bool`: If `true`, the function will block waiting for a keystroke.
+    (**Default**: `true`)
+- `exitkey::Symbol`: A symbol that defines the key that leads to the application
+    termination.
+    (**Default**: `:F1`)
+"""
+function app_main_loop_iteration(; block::Bool = true, exitkey::Symbol = :F1)
+    k = getkey(; block)
+    @emit tui keypressed (; keystroke = k)
+
+    # If the signal destroyed the TUI, we should just exit.
+    !tui.initialized && return false
+
+    # Check if the keystroke must be passed or if the signal hijacked it.
+    if !@get_signal_property(tui, keypressed, block, false)
+        if _check_keystroke(k, exitkey)
+            return false
+        else
+            process_keystroke(k)
+        end
+    end
+
+    @delete_signal_property tui keypressed block
+
+    tui_update()
+
+    return true
 end
 
 ############################################################################################
