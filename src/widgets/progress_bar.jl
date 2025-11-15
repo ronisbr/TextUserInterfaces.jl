569f6d52a0e0a63104bf56133144e2da04d61de2
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-11-15 13:34:47 -0300

    :wrench: Improve progress bar widget

--- src/widgets/progress_bar.jl
+++ src/widgets/progress_bar.jl
@@ -15,11 +15,9 @@ export set_value!
 ############################################################################################
 
 @widget mutable struct WidgetProgressBar
-    border::Bool = false
     show_value::Bool = false
+    style::Symbol = :bar
     value::Float64 = 0.0
-    title::String = ""
-    title_alignment::Symbol = :l
 end
 
 ############################################################################################
@@ -33,32 +31,28 @@ end
 function create_widget(
     ::Val{:progress_bar},
     layout::ObjectLayout;
-    border::Bool = false,
     show_value::Bool = false,
+    style::Symbol = :bar,
     value::Number = 0.0,
-    title::String = "",
-    title_alignment::Symbol = :l,
     theme::Theme = tui.default_theme
 )
 
     # Create the widget.
     progress_bar = WidgetProgressBar(;
         id               = reserve_object_id(),
-        border           = border,
         layout           = layout,
         show_value       = show_value,
+        style            = style,
         value            = value,
         theme            = theme,
-        title            = title,
-        title_alignment  = title_alignment,
         horizontal_hints = Dict(:width  => 30),
-        vertical_hints   = Dict(:height => border ? 3 : 1),
+        vertical_hints   = Dict(:height => 1),
     )
 
     @log DEBUG "create_widget" """
     WidgetInputField created:
       ID         = $(progress_bar.id)
-      Border     = $(progress_bar.border)
+      Style      = $(progress_bar.style)
       Show value = $(progress_bar.show_value)"""
 
     # Return the created container.
@@ -66,76 +60,12 @@ function create_widget(
 end
 
 function redraw!(widget::WidgetProgressBar)
-    @unpack buffer, border, show_value, value, width, title, title_alignment, theme = widget
+    NCurses.wclear(widget.buffer)
 
-    value = clamp(value, 0, 100)
-
-    NCurses.wclear(buffer)
-
-    Δy = 0
-    Δx = 0
-
-    if border
-        NCurses.wborder(buffer)
-
-        # Check if we need to draw the title.
-        if !isempty(title)
-            tw = textwidth(title)
-
-            Δxt = if title_alignment == :r
-                max(width - tw - 1, 0)
-            elseif title_alignment == :c
-                max(div(width - tw - 2, 2), 0)
-            else
-                1
-            end
-
-            NCurses.mvwprintw(buffer, 0, Δxt, title)
-        end
-
-        Δx = 1
-        Δy = 1
-        width -= 2
-    end
-
-    # Compute the number of spaces with and without decoration.
-    num_with_color = round(Int, width * value / 100)
-    num_without_color = width - num_with_color
-
-    # Draw the background.
-    @ncolor (theme.default) buffer begin
-        NCurses.mvwprintw(buffer, Δy, Δx, " "^width)
-    end
-
-    # Draw the progress.
-    @ncolor (theme.default | NCurses.A_REVERSE) buffer begin
-        NCurses.mvwprintw(buffer, Δy, Δx, " "^num_with_color)
-    end
-
-    # Draw the progress value, if requested, considering the correct colors.
-    if show_value
-        progress_str = "$(round(Int, value)) %"
-        lstr = length(progress_str)
-        str_pos = floor(Int, (width - lstr) / 2)
-
-        @inbounds for i in 1:length(progress_str)
-            pos_i = str_pos + (i - 1)
-            color = theme.default
-
-            if pos_i < num_with_color
-                color |= NCurses.A_REVERSE
-            end
-
-            @ncolor color buffer begin
-                c = progress_str[i - 1 + begin]
-
-                if c == '%'
-                    NCurses.mvwprintw(buffer, Δy, pos_i, "%%")
-                else
-                    NCurses.mvwprintw(buffer, Δy, pos_i, string(c))
-                end
-            end
-        end
+    if widget.style == :line
+        _progress_bar__draw_with_line_sytle!(widget)
+    else
+        _progress_bar__draw_with_bar_style!(widget)
     end
 
     return nothing
@@ -159,5 +89,99 @@ Set the `value` of the `progress_bar`.
 function set_value!(progress_bar::WidgetProgressBar, value::Number)
     progress_bar.value = clamp(value, 0, 100)
     request_update!(progress_bar)
+    return nothing
+end
+
+############################################################################################
+#                                    Private Functions                                     #
+############################################################################################
+
+"""
+    _progress_bar__draw_with_bar_style!(widget::WidgetProgressBar) -> Nothing
+
+Draw the progress bar `widget` using the bar style.
+"""
+function _progress_bar__draw_with_bar_style!(widget::WidgetProgressBar)
+    @unpack buffer, show_value, value, width, theme = widget
+
+    value = clamp(value, 0, 100)
+
+    # Compute the number of spaces with decoration.
+    num_with_color = round(Int, width * value / 100)
+
+    # Draw the background.
+    @ncolor (theme.default) buffer begin
+        NCurses.mvwprintw(buffer, 0, 0, " "^width)
+    end
+
+    # Draw the progress.
+    @ncolor (theme.default | NCurses.A_REVERSE) buffer begin
+        NCurses.mvwprintw(buffer, 0, 0, " "^num_with_color)
+    end
+
+    # Draw the progress value, if requested, considering the correct colors.
+    if show_value
+        progress_str = "$(round(Int, value)) %"
+        lstr = length(progress_str)
+        str_pos = floor(Int, (width - lstr) / 2)
+
+        @inbounds for i in 1:length(progress_str)
+            pos_i = str_pos + (i - 1)
+            color = theme.default
+
+            if pos_i < num_with_color
+                color |= NCurses.A_REVERSE
+            end
+
+            @ncolor color buffer begin
+                c = progress_str[i - 1 + begin]
+                NCurses.mvwprintw(buffer, 0, pos_i, c == '%' ? "%%" : string(c))
+            end
+        end
+    end
+
+    return nothing
+end
+
+"""
+    _progress_bar__draw_with_line_sytle!(widget::WidgetProgressBar) -> Nothing
+
+Draw the progress bar `widget` using the line style.
+"""
+function _progress_bar__draw_with_line_sytle!(widget::WidgetProgressBar)
+    @unpack buffer, show_value, value, width, theme = widget
+
+    bar_width = show_value ? width - 6 : width
+
+    # If `bar_width` is less than or equal to zero, there is no space to draw the bar.
+    bar_width ≤ 0 && return nothing
+
+    value = clamp(value, 0, 100)
+
+    # Compute the number of spaces with decoration.
+    num_with_color = round(Int, bar_width * value / 100)
+
+    # Draw the background.
+    @ncolor theme.default buffer begin
+        NCurses.mvwprintw(buffer, 0, 0, "━"^(bar_width))
+        NCurses.mvwprintw(buffer, 0, bar_width, " "^(width - bar_width))
+    end
+
+    # Draw the progress.
+    @ncolor theme.selected buffer begin
+        NCurses.mvwprintw(buffer, 0, 0, "━"^num_with_color)
+    end
+
+    # Draw the progress value, if requested, considering the correct colors.
+    if show_value
+        progress_str = "$(round(Int, value)) %%"
+        lstr         = length(progress_str)
+        str_pos      = width - lstr + 1
+
+        @ncolor theme.default buffer begin
+            NCurses.mvwprintw(buffer, 0, str_pos, progress_str)
+        end
+    end
+
     return nothing
 end
