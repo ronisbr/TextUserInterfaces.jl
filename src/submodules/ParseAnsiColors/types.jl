be223cfb70e4f7456d5569aafc171d3d2e16fd12
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-11-21 10:29:31 -0300

    :books: Improve docs in ParseAnsiColors submodule

--- src/submodules/ParseAnsiColors/types.jl
+++ src/submodules/ParseAnsiColors/types.jl
@@ -4,7 +4,24 @@
 #
 ############################################################################################
 
-# This struct describe the decoration of a string.
+"""
+    struct Decoration
+
+Store the decoration of a text.
+
+# Fields
+
+- `foreground::Int`: Foreground color index.
+    (**Default** = 7)
+- `background::Int`: Background color index.
+    (**Default** = 0)
+- `bold::Bool`: If `true`, the text is bold.
+    (**Default** = `false`)
+- `underline::Bool`: If `true`, the text is underlined.
+    (**Default** = `false`)
+- `reversed::Bool`: If `true`, the foreground and background colors are reversed.
+    (**Default** = `false`)
+"""
 @kwdef struct Decoration
     foreground::Int = 7
     background::Int = 0
