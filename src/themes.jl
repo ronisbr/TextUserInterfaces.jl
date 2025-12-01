599d70f4472795c19fe64cc1eaafedba48c4fffc
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-12-01 16:26:47 -0300

    :sparkles: Add option to set NCurses' default colors

--- src/themes.jl
+++ src/themes.jl
@@ -33,6 +33,36 @@ function set_default_theme_color!(key::Symbol, color::Int)
     return nothing
 end
 
+"""
+    set_ncurses_default_colors(foreground::Union{Int, Symbol}, background::Union{Int, Symbol}) -> Nothing
+
+Set the default ncurses colors to `foreground` and `background`. The arguments can be either
+the symbol specifying the color or the color index.
+"""
+function set_ncurses_default_colors(foreground::Symbol, background::Symbol)
+    # Get the index related to the selected colors.
+    foreground_id = _get_color_index(foreground)
+    background_id = _get_color_index(background)
+
+    return set_ncurses_default_colors(foreground_id, background_id)
+end
+
+function set_ncurses_default_colors(foreground::Integer, background::Symbol)
+    background_id = _get_color_index(background)
+    return set_ncurses_default_colors(foreground, background_id)
+end
+
+function set_ncurses_default_colors(foreground::Symbol, background::Integer)
+    foreground_id = _get_color_index(foreground)
+    return set_ncurses_default_colors(foreground_id, background)
+end
+
+function set_ncurses_default_colors(foreground::Integer, background::Integer)
+    NCurses.assume_default_colors(foreground, background)
+    tui_update(force = true)
+    return nothing
+end
+
 ############################################################################################
 #                                    Private Functions                                     #
 ############################################################################################
@@ -43,12 +73,12 @@ end
 Fill `theme` with the default theme values.
 """
 function _fill_with_default_theme!(theme)
-    theme[:default]   = ncurses_color(:white, :black)
-    theme[:error]     = ncurses_color(:red, :black)
+    theme[:default]   = ncurses_color(:white, -1)
+    theme[:error]     = ncurses_color(:red, -1)
     theme[:highlight] = ncurses_color(; reversed = true)
-    theme[:selected]  = ncurses_color(:yellow, :black)
-    theme[:border]    = ncurses_color(:white, :black)
-    theme[:title]     = ncurses_color(:white, :black; bold = true)
+    theme[:selected]  = ncurses_color(:yellow, -1)
+    theme[:border]    = ncurses_color(:white, -1)
+    theme[:title]     = ncurses_color(:white, -1; bold = true)
 
     return nothing
 end
