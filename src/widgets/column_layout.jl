9f7391e35c209d3e17fbcc34132cb0582f705369
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-01-04 21:19:05 -0300

    :sparkles: Add widget ColumnLayout

---
+++ src/widgets/column_layout.jl
@@ -0,0 +1,116 @@
+## Description #############################################################################
+#
+# Align widgets in a column layout.
+#
+############################################################################################
+
+############################################################################################
+#                                        Constants                                         #
+############################################################################################
+
+# Default anchors for the column layout.
+_COLUMN_LAYOUT__DEFAULT_TOP_ANCHOR    = Anchor(:parent, :top)
+_COLUMN_LAYOUT__DEFAULT_BOTTOM_ANCHOR = Anchor(:parent, :bottom)
+_COLUMN_LAYOUT__DEFAULT_LEFT_ANCHOR   = Anchor(:parent, :left)
+_COLUMN_LAYOUT__DEFAULT_RIGHT_ANCHOR  = Anchor(:parent, :right)
+
+############################################################################################
+#                                        Structure                                         #
+############################################################################################
+
+struct ColumnLayout <: ComposedWidget
+    container::WidgetContainer
+    widgets::Vector{Widget}
+
+    # == Configurations ====================================================================
+    expander::Int
+end
+
+############################################################################################
+#                                        Widget API                                        #
+############################################################################################
+
+function create_widget(
+    ::Val{:column_layout},
+    layout::ObjectLayout;
+    expander::Union{Int, Nothing} = nothing,
+    theme::Theme = Theme(),
+    widgets::AbstractVector
+)
+    if isnothing(expander)
+        expander = length(widgets)
+    end
+
+    container     = create_widget(Val(:container), layout; theme = theme)
+    column_layout = ColumnLayout(container, widgets, expander)
+
+    return column_layout
+end
+
+############################################################################################
+#                                      Container API                                       #
+############################################################################################
+
+# We need a custom function when adding the column layout to a container because we first
+# need the main container added to a parent before creating the other components.
+function add_widget!(parent::WidgetContainer, column_layout::ColumnLayout)
+    @unpack container, expander, widgets = column_layout
+
+    add_widget!(parent, container)
+
+    last_widget = nothing
+
+    for (i, widget) in enumerate(widgets)
+        top_anchor = isnothing(last_widget) ?
+            _COLUMN_LAYOUT__DEFAULT_TOP_ANCHOR :
+            Anchor(last_widget, :bottom)
+
+        layout = ObjectLayout(;
+            left_anchor  = _COLUMN_LAYOUT__DEFAULT_LEFT_ANCHOR,
+            right_anchor = _COLUMN_LAYOUT__DEFAULT_RIGHT_ANCHOR,
+            top_anchor   = top_anchor,
+        )
+
+        widget.layout = layout
+        add_widget!(container, widget)
+
+        last_widget = widget
+
+        i >= expander && break
+    end
+
+    last_widget = nothing
+
+    for (i, widget) in Iterators.reverse(enumerate(widgets))
+        i <= expander && break
+
+        bottom_anchor = isnothing(last_widget) ?
+            _COLUMN_LAYOUT__DEFAULT_BOTTOM_ANCHOR :
+            Anchor(last_widget, :top)
+
+        layout = ObjectLayout(;
+            left_anchor   = _COLUMN_LAYOUT__DEFAULT_LEFT_ANCHOR,
+            right_anchor  = _COLUMN_LAYOUT__DEFAULT_RIGHT_ANCHOR,
+            bottom_anchor = bottom_anchor,
+        )
+
+        widget.layout = layout
+        add_widget!(container, widget)
+
+        last_widget = widget
+    end
+
+    return nothing
+end
+
+function remove_widget!(parent::WidgetContainer, column_layout::ColumnLayout)
+    @unpack container = column_layout
+    remove_widget!(parent, container)
+    return nothing
+end
+
+############################################################################################
+#                                         Helpers                                          #
+############################################################################################
+
+@create_widget_helper column_layout
